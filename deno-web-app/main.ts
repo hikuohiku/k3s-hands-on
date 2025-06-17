9fbd630f21eebfb59d06abdbe25d7fe88f096050
hikuohiku <hikuohiku@gmail.com>
2025-06-18 00:56:45 +0900

    deprecated対応

--- deno-web-app/main.ts
+++ deno-web-app/main.ts
@@ -1,5 +1,3 @@
-import { serve } from "https://deno.land/std@0.203.0/http/server.ts";
-
 const handler = (_req: Request): Response => {
   return new Response("Hello, Deno!", {
     headers: { "content-type": "text/plain" },
@@ -7,4 +5,4 @@ const handler = (_req: Request): Response => {
 };
 
 console.log("Server running on http://localhost:8000/");
-await serve(handler, { port: 8000 });
+await Deno.serve({ port: 8000 }, handler);
